882aecb24c43ed53b54bd83e043849582d75cd7d
anutchai <anuschai.bsod@gmail.com>
2025-09-19 11:21:52 +0700

    add new schedule

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -23,5 +23,5 @@ test('facebook', async ({ page }) => {
 });
 
 test('ig', async ({ page }) => {
-  await page.goto('https//www.instagram.com/');
+  await page.goto('https://www.instagram.com/');
 });
